50f189e65775460c252b9566800d1d3c47ed1145
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-11-23 16:49:35 -0300

    feat: criando login_view e ajustando a view do register

--- core/views/register_view.py
+++ core/views/register_view.py
@@ -1,27 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from core.forms import RegisterForm
 from core.models import Profile
+from django.contrib import messages
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            # Cria o usuário
-            user = form.save()
+    form = RegisterForm(request.POST)
 
-            # Após a criação, associamos esse usuário ao perfil
-            Profile.objects.create(user=user)
-
-            # Loga automaticamente o usuário após o registro
-            login(request, user)
-
-            # Redireciona para a página de perfil ou página inicial
-            # Ajuste para o nome da view de perfil
-            return redirect('profile_view')
-
-    else:
-        form = UserCreationForm()
+    if form.is_valid():
+        form.save()
+        print("Registrado com sucesso")
+        return redirect('core:login')
 
     return render(request, 'register.html', {'form': form})
