fce7c8ee38acb3d95a47cfef44b789e0fa321561
Sezimar Jr <sezimar_jrcs@hotmail.com>
2024-11-27 07:45:36 -0300

    fix: corrigindo pagina de feed

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from core.models.post import Post
+from .models import Profile
+
 # Register your models here.
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'location', 'birth_date',
+                    'avatar')  # Campos que você quer ver na lista
+    # Campos para pesquisa
+    search_fields = ('user__username', 'bio', 'location')
+    list_filter = ('location',)  # Filtros para a barra lateral do admin
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content', 'created_at')
--- core/models/profile.py
+++ core/models/profile.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ValidationError
@@ -18,7 +19,22 @@ class Profile(models.Model):
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     avatar = models.ImageField(
-        upload_to='avatar', blank=True, validators=[validate_image])
+        upload_to='avatars', blank=True, validators=[validate_image], default='images/user_placeholder.png')
+
+    def save(self, *args, **kwargs):
+        # Verifica se o avatar foi alterado
+        if self.pk:  # Verifica se já existe um perfil no banco de dados
+            old_profile = Profile.objects.get(
+                pk=self.pk)  # Recupera o perfil atual
+            old_avatar = old_profile.avatar
+
+            # Se o avatar foi alterado e não for a imagem padrão
+            if old_avatar != self.avatar and old_avatar != 'images/user_placeholder.png':
+                # Deleta a imagem antiga
+                if os.path.isfile(old_avatar.path):  # Verifica se o arquivo existe
+                    os.remove(old_avatar.path)
+
+        super(Profile, self).save(*args, **kwargs)  # Salva o novo perfil
 
     def __str__(self):
         return self.user.username
